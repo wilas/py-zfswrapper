4251fbff7ccad9cf0f86ef45fbcf6a545c21a902
wilas <limak.kamwil@gmail.com>
2013-01-20 22:41:11 +0100

    add setup.py

--- zfswrapper.py
+++ zfswrapper.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# src page: github.com/wilas/zfs-soup/
+# src page: github.com/wilas/py-zfswrapper
 #
 
 """
